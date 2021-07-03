3b3a91f379a0961fc96bfc7835cafe17564ea7b8
Adan Florez <adan.florezb@gmail.com>
2021-07-03 14:55:14 -0500

    feat: theme config

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,9 @@
 import Vue from "vue"
 import Vuetify from "vuetify/lib/framework"
+import dark from "@/plugins/vuetify/theme"
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: { dark: true, themes: { dark } }
+})
