8da0cdecd00589a7c29ac34154ce87c61637f5d0
Adan Florez <adan.florezb@gmail.com>
2021-07-03 16:16:42 -0500

    feat: login component

---
+++ src/components/forms/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="pa-2 mx-auto" max-width="344" elevation="2">
+    <v-card-title class="justify-center" primary-title
+      ><h3>Iniciar sesión</h3></v-card-title
+    >
+    <v-form v-model="isFormValid">
+      <v-container class="d-flex flex-column justify-center">
+        <v-text-field
+          label="Correo"
+          :rules="[rules.required, rules.email]"
+          filled
+          append-icon="mdi-email"
+        ></v-text-field>
+        <v-text-field
+          label="Contraseña"
+          filled
+          type="password"
+          append-icon="mdi-eye-outline"
+          :rules="[rules.required]"
+        ></v-text-field>
+        <v-btn class="ma-2" :disabled="!isFormValid" color="secondary">
+          Entrar
+        </v-btn>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import rules from "@/plugins/vuetify/rules"
+
+export default Vue.extend({
+  data: () => ({
+    rules,
+    isFormValid: false
+  })
+})
+</script>
+
+<style scoped></style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,28 +2,7 @@
   <v-container fill-height>
     <v-row>
       <v-col>
-        <v-card class="pa-2 mx-auto" max-width="344" elevation="2">
-          <v-card-title class="justify-center" primary-title
-            ><h3>Iniciar sesión</h3></v-card-title
-          >
-          <v-form>
-            <v-container class="d-flex flex-column justify-center">
-              <v-text-field
-                label="Correo"
-                filled
-                append-icon="mdi-email"
-              ></v-text-field>
-
-              <v-text-field
-                label="Contraseña"
-                filled
-                type="password"
-                append-icon="mdi-eye-outline"
-              ></v-text-field>
-              <v-btn class="ma-2" color="secondary"> Entrar </v-btn>
-            </v-container>
-          </v-form>
-        </v-card>
+        <Login />
       </v-col>
     </v-row>
   </v-container>
@@ -31,8 +10,10 @@
 
 <script lang="ts">
 import Vue from "vue"
+import Login from "@/components/forms/Login.vue"
 
 export default Vue.extend({
+  components: { Login },
   name: "Home"
 })
 </script>
