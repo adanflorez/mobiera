5db29631c2bf6457b55195fa78ab6a405ce1cccb
Adan Florez <adan.florezb@gmail.com>
2021-07-04 20:34:06 -0500

    refactor: toolbar

--- src/layouts/main/Main.vue
+++ src/layouts/main/Main.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="main-wrapper">
-    <v-app-bar color="deep-purple accent-4" dense dark>
-      <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list nav>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="logoutUser">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>Cerrar sesión</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ $route.name }}
+        </router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" dark v-bind="attrs" v-on="on">
-            {{ userName }}
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item>
-            <v-list-item-title>Perfíl</v-list-item-title>
-          </v-list-item>
-          <v-list-item @click="logoutUser">
-            <v-list-item-title>Cerrar sesión</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn>
+          {{ userName }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
     <router-view></router-view>
   </div>
 </template>
@@ -31,6 +47,13 @@ export default Vue.extend({
   computed: {
     ...mapGetters(["userName"])
   },
+  data: () => ({
+    sidebar: false,
+    menuItems: [
+      { title: "Inicio", path: "/home", icon: "mdi-home" },
+      { title: "Editar perfíl", path: "/edit", icon: "mdi-account" }
+    ]
+  }),
   methods: {
     /**
      * user session ends
